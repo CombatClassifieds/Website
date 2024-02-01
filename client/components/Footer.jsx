8efe4ae085fb3d07e6f7206f4f5a32997fa1ac81
Anthony Meadows <anthonylinux@Kubuntu.OS>
2024-02-01 12:26:45 -0700

    Broken

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -1,5 +1,7 @@
 export default function Footer() {
     return (
-        <footer>This is a footer</footer>
+        <footer className = 'footer'>
+            <p> This is a footer </p>
+        </footer>
     );
 }
