698b1c05a99245c9eb0eae241010b4f150b674d9
Mslonoff <matthew.slonoff@gmail.com>
2024-02-01 07:32:33 -0500

    starting categories

--- client/components/Content.jsx
+++ client/components/Content.jsx
@@ -1,7 +1,10 @@
 export default function Content() {
     return (
-        <div>
+        <div id="content">
             <img src="https://cdn11.bigcommerce.com/s-cyn7ecnpcb/images/stencil/608x608/products/2723/3064/6932__13776.1653424122.jpg?c=1"></img>
+            <div id="categories">
+                <a className="theWordCategories">CATEGORIES</a>
+            </div>
         </div>
     );
 }
