1547f9508372d1e20d78cb5d5de076bca883eca4
Anthony Meadows <anthonylinux@Kubuntu.OS>
2024-02-01 19:22:30 -0700

    linted

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,12 +1,11 @@
 export default function Header() {
-    return (
-        <header className="header">
-            <h1>Combat Classifieds</h1>
-            <div className="search">
-                <input type="search" />
-                <button>Search</button>
-            </div>
-        </header>
-    );
+  return (
+    <header className="header">
+      <h1>Combat Classifieds</h1>
+      <div className="search">
+        <input type="search" />
+        <button>Search</button>
+      </div>
+    </header>
+  );
 }
-
