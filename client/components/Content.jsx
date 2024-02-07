4a92df7954bbfd51f79cbd804aa8f337a38f6ea7
Anthony Meadows <anthonylinux@Kubuntu.OS>
2024-02-07 11:57:02 -0700

    Added some content and working buttons

--- client/components/Content.jsx
+++ client/components/Content.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartPlus, faBitcoinSign } from "@fortawesome/free-solid-svg-icons";
 
 export default function Content() {
   return (
-    <div class="flex">
+    <div className="flex">
       <AccordianMenu />
       <ItemDetails />
     </div>
@@ -10,18 +12,133 @@ export default function Content() {
 }
 
 function ItemDetails() {
+  let tempItem = {
+    img: `./images/lego_secret_tank.jpeg`,
+    name: `L3g0 REDACTED of INF&^!KJM`,
+    NSN: `9X237REDACTED73--74`,
+    price: `230.17`,
+  };
+
+  const handleAddToCart = () => {
+    alert("Item added to cart!");
+  };
+
+  const handleBitcoinClick = () => {
+    alert("USD WORTH")
+  }
+
   return (
-    <>
-      <div id="itemDetails">
-        <img src="./images/lego_secret_tank.jpeg" />
-        <div id="itemSpecs">THIS IS WORDS</div>
+    <div id="itemInfo">
+      <img src={tempItem.img} alt="Item" />
+      <div id="itemSpecs">
+        <div id="itemName">{tempItem.name}</div>
+        <div id="sku">NSN: {tempItem.NSN}</div>
+        <div id="itemPrice" onClick={handleBitcoinClick}>
+          <FontAwesomeIcon icon={faBitcoinSign} />
+            {' '+ tempItem.price}
+        </div>
+        <div id="addToCart">
+          <button onClick={handleAddToCart}>
+            <FontAwesomeIcon icon={faCartPlus} />
+          </button>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
 
 function AccordianMenu() {
   const [selectedCategory, setSelectedCategory] = useState(null);
+
+  let categoryItems = [
+    "OCS & Ranger School",
+    "ROTC/JROTC Items",
+    "Clothing",
+    "Uniform Accessories",
+    "Crests",
+    "Eyewear",
+    "Field Equipment",
+    "Flashlights",
+    "Footwear & Accessories",
+    "Gifts, Novelty, Books/Manuals",
+    "Headwear",
+    "Knives & Tools",
+    "New Arrivals",
+    "Tactical Gear",
+    "Weapons Accessories",
+    "Clearance",
+  ];
+  
+  let popularBrands = [
+    "Propper",
+    "CamelBak",
+    "Tru Spec",
+    "Rite in the Rain",
+    "Benchmade",
+    "Oakley",
+    "Raine",
+    "Sua Sponte",
+    "Gerber",
+    "Tasmanian Tiger",
+    "View All",
+  ];
+  
+  let ocsRangerSchoolItems = [
+    "All OCS & Ranger School",
+    "OCS - Click Here",
+    "Ranger School - Click Here",
+  ];
+  
+  let clothingItems = [
+    "All Clothing",
+    "Belts",
+    "Hot Weather OCP Uniforms",
+    "Gloves",
+    "Jackets, Rainwear",
+    "Kids",
+    "P.T.",
+    "T-Shirts, Sweatshirts",
+    "Underwear",
+    "Uniforms-Female",
+    "Uniforms",
+    "Vests",
+  ];
+  
+  let uniformAccessories = [
+    "All Uniform Accessories",
+    "Personalized Items",
+    "AGSU-Pinks & Greens",
+    "Army OCP Badges",
+    "Army OCP Patches",
+    "Army OCP Rank",
+    "Army OCP Scrolls",
+    "Army OCP Tabs",
+    "Air Force OCP Badges",
+    "Air Force OCP Patches",
+    "Air Force OCP Rank",
+    "Air Force Uniform Items",
+    "Badges",
+    "Bars",
+    "Branch",
+    "Buttons",
+    "Cap Devices",
+    "Devices",
+    "Flashes",
+    "Medals",
+    "Ovals",
+    "Parade Items",
+    "Patches",
+    "Rank",
+    "Ribbons",
+    "Scrolls",
+    "Shoulder Boards",
+    "Service Stripes",
+    "Shoulder Cords",
+    "Tabs",
+  ];
+  
+  let eyewear = ["All Eyewear", "Accessories", "Glasses", "Goggles", "Rx"];
+  
 
   const handleAccordionClick = (category) => {
     setSelectedCategory(selectedCategory === category ? null : category);
@@ -31,21 +148,18 @@ function AccordianMenu() {
     <div id="categoriesAndBrands">
       <div id="categories">
         <a className="theWordCategories">CATEGORIES</a>
-          <div id="categoriesList">
-            {categoryItems.map((category) => (
-              <div key={category}>
-                <button
-                  className={`accordion ${
-                    selectedCategory === category ? "active" : ""
-                  }`}
-                  onClick={() => handleAccordionClick(category)}
-                >
-                  {category}
-                  {/* Render special symbol for accordion items */}
-                  {category === "OCS & Ranger School" || category === "Clothing" || category === "Uniform Accessories" || category === "Eyewear" || category === "Field Equipment" || category === "Flashlights" || category === "Footwear & Accessories" || category === "Gifts, Novelty, Books/Manuals" || category === "Headweawr" || category === "Knives & Tools" || category === "Tactical Gear" || category === "Weapons Accessories" ? (
-                    <span className="accordionSymbol">^</span>
-                  ) : null}
-                </button>
+
+        <div id="categoriesList">
+          {categoryItems.map((category) => (
+            <div key={category}>
+              <button
+                className={`accordion ${
+                  selectedCategory === category ? "active" : ""
+                }`}
+                onClick={() => handleAccordionClick(category)}
+              >
+                {category}
+              </button>
 
               {/* Render accordion items based on selected category */}
               {selectedCategory === category && (
@@ -57,88 +171,26 @@ function AccordianMenu() {
                         {item}
                       </button>
                     ))}
-                    {/* Render 'Clothing' items */}
-                    {category === "Clothing" &&
-                      clothingItems.map((item) => (
-                        <button className="accordionItem" key={item}>
-                          {item}
-                        </button>
-                      ))}
-                    {/* Render 'Uniform Accessories' items */}
-                    {category === "Uniform Accessories" &&
-                      uniformAccessories.map((item) => (
-                        <button className="accordionItem" key={item}>
-                          {item}
-                        </button>
-                      ))}
-                    {/* Render 'Eyewear' items */}
-                    {category === "Eyewear" &&
-                      eyewear.map((item) => (
-                        <button className="accordionItem" key={item}>
-                          {item}
-                        </button>
-                      ))}
-                    {/* Render 'Field Equipment items */}
-                    {category === "Field Equipment" &&
-                      fieldEquipment.map((item) => (
-                        <button className="accordionItem" key={item}>
-                          {item}
-                        </button>
-                      ))}
-                    {/* Render 'Flashlights' items*/}
-                    {category === "Flashlights" &&
-                      flashlights.map((item) => (
-                        <button className="accordionItem" key={item}>
-                          {item}
-                        </button>
-                      ))}
-                    {/* Render 'Footwear & Accessories' items*/}
-                    {category === "Footwear & Accessories" &&
-                      footwearAndAccessories.map((item) => (
-                        <button className="accordionItem" key={item}>
-                          {item}
-                        </button>
-                      ))}
-                    {/*Render 'Gifts, Novelty, Books/Manuals' items*/}
-                    {category === "Gifts, Novelty, Books/Manuals" &&
-                      gifts.map((item) => (
-                        <button className="accordionItem" key={item}>
-                          {item}
-                        </button>
-                      ))}
-                    {/*Render 'Headwear' items*/}
-                    {category === "Headwear" &&
-                      headwear.map((item) => (
-                        <button className="accordionItem" key={item}>
-                          {item}
-                        </button>
-                      ))}
-                    {/*Render 'Knives & Tools*/}
-                    {category === "Knives & Tools" &&
-                      knivesAndTools.map((item) => (
-                        <button className="accordionItem" key={item}>
-                          {item}
-                        </button>
-                      ))}
-                    {/*Render 'Tactical Gear' items*/}
-                    {category === "Tactical Gear" &&
-                      tacticalGear.map((item) => (
-                        <button className="accordionItem" key={item}>
-                          {item}
-                        </button>
-                      ))}
-                    {/*Render 'Weapons Accessories' items*/}
-                    {category === "Weapons Accessories" &&
-                      weaponsAccessories.map((item) => (
-                        <button className="accordionItem" key={item}>
-                          {item}
-                        </button>
-                      ))}
-                  </div>
-                )}
-              </div>
-            ))}
-          </div>
+
+                  {/* Render 'Clothing' items */}
+                  {category === "Clothing" &&
+                    clothingItems.map((item) => (
+                      <button className="accordionItem" key={item}>
+                        {item}
+                      </button>
+                    ))}
+                  {/* Render 'Uniform Accessories' items */}
+                  {category === "Uniform Accessories" &&
+                    uniformAccessories.map((item) => (
+                      <button className="accordionItem" key={item}>
+                        {item}
+                      </button>
+                    ))}
+                  {/* Render 'Eyewear' items */}
+                </div>
+              )}
+            </div>
+          ))}
         </div>
       </div>
 
@@ -156,184 +208,3 @@ function AccordianMenu() {
     </div>
   );
 }
-
-let fieldEquipment = [
-    "All Field Equipment",
-    "ALICE< MOLLE GEAR",
-    "Bags, Packs, Cases",
-    "Emergency Preparedness",
-    "Essential Emergency Items",
-    "Field Gear",
-    "Field Supplies",
-    "First Aid",
-    "Hydration",
-    "Navigation",
-    "Notebooks Cards",
-    "Rappelling",
-    "Sleeping",
-    "Survival",
-    "Wallets/ID Holders",
- ];
- let flashlights = [
-    "All Flashlights",
-    "Accessories",
-    "Chemlights",
-    "Headlamps",
-    "LED",
- ];
- let footwearAndAccessories = [
-    "All Footwear & Accessories",
-    "Boot Accessories",
-    "AR670-1 Compliant",
-    "Boot Care",
-    "Boots",
-    "Foot Care",
-    "Insoles",
-    "Socks",
-    "Shoes",
- ];
- let gifts = [
-    "All Gifts, Novelty, Books/Manuals",
-    "Books/Manuals",
-    "Clothing & Hats",
-    "Coins",
-    "Decals, Stickers",
-    "Flags",
-    "Games, Toys",
-    "Magnets",
-    "Gift Cards",
-    "Jewelry",
-    "Key Rings",
-    "KIA, POW",
-    "Morale Patches & Lugage Tags",
-    "Mugs",
-    "Ornaments",
-    "Watches",
-];
-let headwear = [
-    "All Headwear",
-    "Balaclavas, Hoods, Masks",
-    "Berets",
-    "Boonie Hats",
-    "Campaign Hats and Accessories",
-    "Hat Accessories",
-    "Helmets and Accessories",
-    "Novelty",
-    "Patrol Caps",
-    "Veils, Scarves",
-    "Watch Caps, Beenies",
-];
-let knivesAndTools = [
-    "All Knives & Tools",
-    "Fixed Blades",
-    "Folding Knives",
-    "Machetes",
-    "Sharpeners & Acc",
-    "Sheaths",
-    "Tools",
-];
-  let tacticalGear = [
-    "All Tactical Gear",
-    "Bags, Packs",
-    "Duty Gear",
-    "Ghillie Suits/Camo Acc",
-    "Holsters, Duty Gear",
-    "Optics",
-    "Protective Gear, Restraints",
-    "Vests",
-];
-let weaponsAccessories = [
-    "All WEapons Accessories",
-    "Bipods",
-    "Cleaning Kits & Accessories",
-    "Grips",
-    "Holsters",
-    "Lanyards",
-    "Magazines & Accessories",
-    "Slings",
-    "Weapons Accessories",
-];
-let categoryItems = [
-  "OCS & Ranger School",
-  "ROTC/JROTC Items",
-  "Clothing",
-  "Uniform Accessories",
-  "Crests",
-  "Eyewear",
-  "Field Equipment",
-  "Flashlights",
-  "Footwear & Accessories",
-  "Gifts, Novelty, Books/Manuals",
-  "Headwear",
-  "Knives & Tools",
-  "New Arrivals",
-  "Tactical Gear",
-  "Weapons Accessories",
-  "Clearance",
-];
-let popularBrands = [
-  "Propper",
-  "CamelBak",
-  "Tru Spec",
-  "Rite in the Rain",
-  "Benchmade",
-  "Oakley",
-  "Raine",
-  "Sua Sponte",
-  "Gerber",
-  "Tasmanian Tiger",
-  "View All",
-];
-let ocsRangerSchoolItems = [
-  "All OCS & Ranger School",
-  "OCS - Click Here",
-  "Ranger School - Click Here",
-];
-let clothingItems = [
-  "All Clothing",
-  "Belts",
-  "Hot Weather OCP Uniforms",
-  "Gloves",
-  "Jackets, Rainwear",
-  "Kids",
-  "P.T.",
-  "T-Shirts, Sweatshirts",
-  "Underwear",
-  "Uniforms-Female",
-  "Uniforms",
-  "Vests",
-];
-let uniformAccessories = [
-  "All Uniform Accessories",
-  "Personalized Items",
-  "AGSU-Pinks & Greens",
-  "Army OCP Badges",
-  "Army OCP Patches",
-  "Army OCP Rank",
-  "Army OCP Scrolls",
-  "Army OCP Tabs",
-  "Air Force OCP Badges",
-  "Air Force OCP Patches",
-  "Air Force OCP Rank",
-  "Air Force Uniform Items",
-  "Badges",
-  "Bars",
-  "Branch",
-  "Buttons",
-  "Cap Devices",
-  "Devices",
-  "Flashes",
-  "Medals",
-  "Ovals",
-  "Parade Items",
-  "Patches",
-  "Rank",
-  "Ribbons",
-  "Scrolls",
-  "Shoulder Boards",
-  "Service Stripes",
-  "Shoulder Cords",
-  "Tabs",
-];
-
-let eyewear = ["All Eyewear", "Accessories", "Glasses", "Goggles", "Rx"];
