56413e27b16c5886c92a9e65744b2b3d98a31d8d
dillontouchard <dillon.touchard@yahoo.com>
2024-02-08 07:56:34 -0800

    Fixed Button Animation

--- client/components/Content.jsx
+++ client/components/Content.jsx
@@ -57,7 +57,7 @@ function AccordianMenu() {
   };
 
   return (
-    <div id="accordianMenu">
+    <div id="categoriesAndBrands">
       <div id="categories">
         <a className="theWordCategories">CATEGORIES</a>
         <div id="categoriesList">
@@ -89,7 +89,8 @@ function AccordianMenu() {
 function Category({ category, currentlySelected, onClick }) {
   const children =
     currentlySelected && typeof category === "object" ? (
-      <div className="children">
+      <div>
+        {/* Render 'OCS & Ranger School' items */}
         {category.children.map((item) => (
           <SubCategory item={item} />
         ))}
@@ -107,6 +108,8 @@ function Category({ category, currentlySelected, onClick }) {
       <button className={classes.join(" ")} onClick={onClick}>
         {typeof category === "string" ? category : category.name}
       </button>
+
+      {/* Render accordion items based on selected category */}
       {children}
     </div>
   );
@@ -114,7 +117,7 @@ function Category({ category, currentlySelected, onClick }) {
 
 function SubCategory({ item }) {
   return (
-    <button key={item} className="accordion">
+    <button className="accordionItem" key={item}>
       {item}
     </button>
   );
