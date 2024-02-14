3b62821e91d20226a7fc9f73e3740879a4a6ff2c
Athan Clark <athan.clark@gmail.com>
2024-02-14 16:26:38 -0500

    prettier

--- src/app/api/suggested/[id]/route.js
+++ src/app/api/suggested/[id]/route.js
@@ -4,9 +4,10 @@ export const dynamic = "force-dynamic";
 
 export async function GET(request, { params: { id } }) {
   let psql = await getPsql();
-  let results = await psql.query("SELECT img, id FROM items WHERE id <> $1 LIMIT 10 OFFSET $1", [
-    id,
-  ]);
+  let results = await psql.query(
+    "SELECT img, id FROM items WHERE id <> $1 LIMIT 10 OFFSET $1",
+    [id]
+  );
   return new Response(JSON.stringify(results.rows), {
     contentType: "application/json",
   });
