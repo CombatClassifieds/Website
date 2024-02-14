017a1c8ac640dd5558e881a3d2d0c65757e3d523
Athan Clark <athan.clark@gmail.com>
2024-02-14 14:16:08 -0500

    smaller suggested items

--- src/app/api/suggested/[id]/route.js
+++ src/app/api/suggested/[id]/route.js
@@ -4,7 +4,7 @@ export const dynamic = "force-dynamic";
 
 export async function GET(request, { params: { id } }) {
   let psql = await getPsql();
-  let results = await psql.query("SELECT img, id FROM items WHERE id <> $1", [
+  let results = await psql.query("SELECT img, id FROM items WHERE id <> $1 LIMIT 10 OFFSET $1", [
     id,
   ]);
   return new Response(JSON.stringify(results.rows), {
